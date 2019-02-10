3a6fa195f59dcd1d839f23414947067859d7ee0f
amalinin-gd <amalinin@griddynamics.com>
2019-02-10 18:13:26 +0100

    Added a demo Context Menu item

--- chrome-player/src/ext_scripts/background.js
+++ chrome-player/src/ext_scripts/background.js
@@ -1,6 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "id": "sampleContextMenu",
+    "title": "Open Chrome Player in a New Tab",
+    "contexts": ["page", "selection", "image", "link"]
+  });
+
   alert('Chrome Player extension has been installed!');
 });
+
+chrome.contextMenus.onClicked.addListener(function() {
+  chrome.tabs.create({ url: "index.html" });
+ })
 
 chrome.bookmarks.onCreated.addListener(function() {
   alert('Better listen to some music!');
