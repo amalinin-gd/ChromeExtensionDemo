e276f6ada4dc9784fc2cca07bfb37cfb6ea20ba1
amalinin-gd <amalinin@griddynamics.com>
2019-02-10 17:22:20 +0100

    Added the Bookmark Created event listener to the Background Script

--- chrome-player/src/ext_scripts/background.js
+++ chrome-player/src/ext_scripts/background.js
@@ -1,3 +1,7 @@
 chrome.runtime.onInstalled.addListener(function() {
   alert('Chrome Player extension has been installed!');
 });
+
+chrome.bookmarks.onCreated.addListener(function() {
+  alert('Better listen to some music!');
+});
