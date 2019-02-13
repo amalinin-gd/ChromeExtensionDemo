0eed98ad45148113823d28e3b877575222afd37e
amalinin-gd <amalinin@griddynamics.com>
2019-02-13 14:48:58 +0100

    Removed excessive code from the content script

--- chrome-player/src/ext_scripts/content.js
+++ chrome-player/src/ext_scripts/content.js
@@ -1,14 +1,12 @@
 function init() {
-  var viewportElement = document.body;
-
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (!request.imageUrl){
         return;
       }
 
-      viewportElement.style.backgroundImage = 'url("' + request.imageUrl + '")';
-      viewportElement.style.backgroundSize = 'contain';
+      document.body.style.backgroundImage = 'url("' + request.imageUrl + '")';
+      document.body.style.backgroundSize = 'contain';
 
       sendResponse({ message: "The wallpaper has been set!" });
     });
